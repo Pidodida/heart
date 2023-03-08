7d2269edd35f8a807824d4bd8a44880343f3b2df
Pidodida <91645251+Pidodida@users.noreply.github.com>
2023-03-08 18:42:43 +0800

    Update heart.py

--- Newfolder/heart.py
+++ Newfolder/heart.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-something_model = pickle.load(open('C:/Users/pidod/Desktop/Newfolder/heart.sav', 'rb'))
+something_model = pickle.load(open('heart.sav', 'rb'))
 with st.sidebar:
     
     selected = option_menu('Heart Disease Prediction System', 
